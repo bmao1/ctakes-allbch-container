7849867796aa2b70ae3bfdfd1396bcd353a3e5f4
Tim Miller <timothy.miller@childrens.harvard.edu>
2021-07-19 19:03:31 -0400

    Update to download script to use aws full path.

--- download_s3_files.sh
+++ download_s3_files.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # May have to also set AWS key parameters here, potentially passed in as tags as well.
+aws=/usr/local/bin/aws
 
-s3_bucket=`aws ec2 describe-tags --filters Name=key,Value=s3_bucket | jq --raw-output '.Tags[0].Value'`
-s3_input_path=`aws ec2 describe-tags --filters Name=key,Value=s3_in | jq --raw-output '.Tags[0].Value'`
+s3_bucket=`$aws ec2 describe-tags --filters Name=key,Values=s3_bucket | jq --raw-output '.Tags[0].Value'`
+s3_input_path=`$aws ec2 describe-tags --filters Name=key,Values=s3_in | jq --raw-output '.Tags[0].Value'`
 
-aws s3 cp s3://${s3_bucket}/${s3_input_path} /data/
+$aws s3 cp s3://${s3_bucket}/${s3_input_path} /data/
 
 cd /data
 for fn in *.csv.encrypted; do
