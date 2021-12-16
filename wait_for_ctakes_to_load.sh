e19e5d1ebe8fc64e9d93084eb86dab5ed330a3ea
bmao1 <logttt@gmail.com>
2021-12-16 18:33:43 -0500

    scale v1

--- wait_for_ctakes_to_load.sh
+++ wait_for_ctakes_to_load.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-while true; do
-    ready=`docker logs $1 2>&1 | grep -c "org.apache.catalina.startup.Catalina.start Server startup"`
-    if [ "$ready" -eq "1" ]; then
-        break
-    fi
-    sleep 2
+for container in $@
+do
+    while [[ $container == nlp* ]]; do
+        ready=`docker logs $container 2>&1 | grep -c "org.apache.catalina.startup.Catalina.start Server startup"`
+        if [ "$ready" -eq "1" ]; then
+            break
+        fi
+        sleep 2
+    done
 done
-
